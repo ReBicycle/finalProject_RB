e990dd8cfc02399b8381b717bc305a9b17ecf8ce
sy2399 <ter194@ajou.ac.kr>
2017-05-26 11:33:12 +0900

    회원가입

--- Rebicycle/src/main/java/org/kosta/rebicycle/controller/MemberController.java
+++ Rebicycle/src/main/java/org/kosta/rebicycle/controller/MemberController.java
@@ -1,5 +1,8 @@
 package org.kosta.rebicycle.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -10,10 +13,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class MemberController {
-	private String uploadPath;
+	private String uploadPath="C://java-kosta//ReBicycle//springmvc21-fileupload-inst//src//main//webapp//resources//upload//";
 	
 	@Resource
 	private MemberService memberService;
@@ -46,9 +50,22 @@ public class MemberController {
 	
 	@RequestMapping(method=RequestMethod.POST, value = "memberRegister.do")
 	public String memberRegister(MemberVO vo, HttpServletRequest request){
-		//System.out.println(vo);
-		uploadPath = "C:\\Users\\소영\\git\\finalProject_RB\\Rebicycle\\src\\main\\webapp\\resources\\upload\\";		
 		
+		System.out.println(vo);
+		MultipartFile file = vo.getUploadFile();
+		
+		uploadPath += "member\\"+file.getOriginalFilename();
+		if(file.isEmpty()==false){
+			File uploadFile = new File(uploadPath);
+			try {
+				file.transferTo(uploadFile);
+				System.out.println("멤버사진 등록완료-회원가입");
+			} catch (IllegalStateException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		vo.setPicture(file.getOriginalFilename());
 		memberService.registerMember(vo);
 		return "redirect:home.do";
 	}

6d87ed6e92ac52dd6ff918a1ecbda4bc4d072a09
sy2399 <ter194@ajou.ac.kr>
2017-05-26 13:29:50 +0900

    test

--- Rebicycle/src/main/java/org/kosta/rebicycle/controller/HomeController.java
+++ Rebicycle/src/main/java/org/kosta/rebicycle/controller/HomeController.java
@@ -1,6 +1,5 @@
 package org.kosta.rebicycle.controller;
 
-import org.kosta.rebicycle.model.vo.BicycleVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;

2bde4301c434c9dad81dbcdeaacd2f3d48848afb
KOSTA <KOSTA@192.168.0.122>
2017-05-26 17:38:37 +0900

    태형

--- Rebicycle/src/main/java/org/kosta/rebicycle/controller/BicycleController.java
+++ Rebicycle/src/main/java/org/kosta/rebicycle/controller/BicycleController.java
@@ -6,8 +6,10 @@ import javax.annotation.Resource;
 
 import org.kosta.rebicycle.model.service.BicycleServiceImpl1;
 import org.kosta.rebicycle.model.vo.BicycleVO;
+import org.kosta.rebicycle.model.vo.CalendarVO;
 import org.kosta.rebicycle.model.vo.MemberVO;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,10 +20,12 @@ public class BicycleController {
 	private BicycleServiceImpl1 serviceImpl1;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "registerBicycle.do")
-	public String registerBicycle(BicycleVO bvo, String memberId, int categoryNo){
+	@Transactional
+	public String registerBicycle(BicycleVO bvo, String memberId, int categoryNo, CalendarVO cvo){
 		bvo.setMemberVO(new MemberVO(memberId));
 		bvo.setCategoryNo(categoryNo);
 		serviceImpl1.registerBicycle(bvo);
+		serviceImpl1.registerPossibleDate(cvo);
 		return "bicycle/bicycle_register_result.tiles";
 	}
 	

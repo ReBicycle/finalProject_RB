59ee97069c667fa126f09a1b5fd35f6d25482076
Administrator <Administrator@192.168.0.116>
2017-05-26 14:54:37 +0900

    junit

---
+++ Rebicycle/src/test/java/org/kosta/rebicycle/test/TestUnit.java
@@ -0,0 +1,24 @@
+package org.kosta.rebicycle.test;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring-model.xml"})
+public class TestUnit {
+@Resource
+private SqlSessionTemplate t;
+
+
+@Test
+public void test(){
+	
+
+System.out.println("하이");
+}
+}

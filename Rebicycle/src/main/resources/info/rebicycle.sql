94fa39e66297a0dbcdbe5fb12658a69005d1b657
KOSTA <KOSTA@192.168.0.122>
2017-05-26 15:54:19 +0900

    대여료제시 완료

--- Rebicycle/src/main/resources/info/rebicycle.sql
+++ Rebicycle/src/main/resources/info/rebicycle.sql
@@ -130,7 +130,9 @@ insert into category(categoryNo, categoryName) values(3, '픽시');
 insert into category(categoryNo, categoryName) values(4, '레코드용');
 insert into category(categoryNo, categoryName) values(5, '어린이용');
 
-insert into category(categoryNo, categoryName) values(7, '기타');
+insert into category(categoryNo, categoryName) values(6, '기타');
 
 select * from bicycle;
+select min(rentPrice) from bicycle;
+select round(avg(rentPrice), 1) from bicycle;
 -----------------------태형-----------------------------------------

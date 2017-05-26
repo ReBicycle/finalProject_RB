9c9077cdb37bef784312b06dbe51d41b6a97909b
Administrator <Administrator@192.168.0.116>
2017-05-26 16:09:26 +0900

    종봉

--- Rebicycle/src/main/resources/info/rebicycle.sql
+++ Rebicycle/src/main/resources/info/rebicycle.sql
@@ -127,7 +127,19 @@ select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.deta
 select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.detail ,b.categoryNo,p.startDay,p.endDay ,bp.photo1,bp.photo2,bp.photo3
 from bicycle b, possible_day p, bicycle_photo bp 
 where address like '%'||'판'||'%' and b.bicycleNo=p.bicycleNo and to_date('2017-05-28')>=p.startDay and to_date('2017-05-29')<=p.endDay and b.bicycleNo=bp.bicycleNo;
+
+
+	select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.detail ,b.categoryNo,p.startDay,p.endDay ,c.photo1,c.photo2,c.photo3
+		from bicycle b, possible_day p, bicycle_photo c  
+		where b.address like '%' ||'판교'|| '%' and b.bicycleNo=p.bicycleNo and to_date('2017-05-28')>=p.startDay and to_date('2017-05-29')<=p.endDay and b.bicycleNo=c.bicycleNo
+ 	
+
+
 --좌표까지 조회
+
+
+
+
 
 ------------종봉----------------------------------------------
 
--- Rebicycle/src/test/java/org/kosta/rebicycle/test/TestUnit.java
+++ Rebicycle/src/test/java/org/kosta/rebicycle/test/TestUnit.java
@@ -1,10 +1,15 @@
 package org.kosta.rebicycle.test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kosta.rebicycle.model.dao.BicycleDAOImpl5;
+import org.kosta.rebicycle.model.vo.BicycleVO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -14,13 +19,33 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class TestUnit {
 @Resource
 private SqlSessionTemplate t;
-@Resource
+
+/*@Resource
 private BicycleDAOImpl5 dao;
+*
+*/
+
+public List<BicycleVO> getBicycleListByAddressAndDay(String address,String startDay,String endDay){
+	Map<String,String> map=new HashMap<String,String>();
+	map.put("address", address);
+	map.put("startDay", startDay);
+	map.put("endDay", endDay);
+	List<BicycleVO> list=t.selectList("bicycle.getBicycleListByAddressAndDay", map);
+	System.out.println(list);
+	return list;
+}
 
 @Test
 public void test(){
 	System.out.println("하이");
-dao.getBicycleListByAddressAndDay("판교", "2017-05-26", "2017-05-28");
+	Map<String,String> map=new HashMap<String,String>();
+	map.put("address", "판교");
+	map.put("startDay", "2017-05-26");
+	map.put("endDay", "2017-05-28");
+	List<BicycleVO> list=t.selectList("bicycle.getBicycleListByAddressAndDay", map);
+	System.out.println(list);
+	
+	
 System.out.println("바이");
 }
 }

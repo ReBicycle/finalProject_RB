139995088e43d4e9f66ab2f11111d4b3e45176b5
Administrator <Administrator@192.168.0.117>
2017-05-27 09:46:11 +0900

    sql

--- Rebicycle/src/main/resources/info/rebicycle.sql
+++ Rebicycle/src/main/resources/info/rebicycle.sql
@@ -130,12 +130,6 @@ select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.deta
 from bicycle b, possible_day p, bicycle_photo bp 
 where address like '%'||'판'||'%' and b.bicycleNo=p.bicycleNo and to_date('2017-05-28')>=p.startDay and to_date('2017-05-29')<=p.endDay and b.bicycleNo=bp.bicycleNo;
 
-<<<<<<< HEAD
-	select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.detail ,b.categoryNo,p.startDay,p.endDay ,c.photo1,c.photo2,c.photo3
-		from bicycle b, possible_day p, bicycle_photo c  
-		where b.address like '%' ||'판교'|| '%' and b.bicycleNo=p.bicycleNo and to_date('2017-05-28')>=p.startDay and to_date('2017-05-29')<=p.endDay and b.bicycleNo=c.bicycleNo
- 	
-=======
 
 	select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.detail ,b.categoryNo,p.startDay,p.endDay ,c.photo1,c.photo2,c.photo3
 		from bicycle b, possible_day p, bicycle_photo c  
@@ -147,7 +141,6 @@ where address like '%'||'판'||'%' and b.bicycleNo=p.bicycleNo and to_date('2017
 
 
 
->>>>>>> branch 'master' of https://github.com/ReBicycle/finalProject_RB.git
 
 
 ------------종봉----------------------------------------------
@@ -166,4 +159,8 @@ insert into category(categoryNo, categoryName) values(6, '기타');
 select * from bicycle;
 select min(rentPrice) from bicycle;
 select round(avg(rentPrice), 1) from bicycle;
+select bicycle_seq.currval from dual;
+
+
+
 -----------------------태형-----------------------------------------

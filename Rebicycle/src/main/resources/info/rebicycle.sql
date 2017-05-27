c6ab96d592d773905025bac8a508514ec10bcd91
Administrator <Administrator@192.168.0.116>
2017-05-27 09:45:25 +0900

    종봉 검색

--- Rebicycle/src/main/resources/info/rebicycle.sql
+++ Rebicycle/src/main/resources/info/rebicycle.sql
@@ -130,12 +130,7 @@ select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.deta
 from bicycle b, possible_day p, bicycle_photo bp 
 where address like '%'||'판'||'%' and b.bicycleNo=p.bicycleNo and to_date('2017-05-28')>=p.startDay and to_date('2017-05-29')<=p.endDay and b.bicycleNo=bp.bicycleNo;
 
-<<<<<<< HEAD
-	select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.detail ,b.categoryNo,p.startDay,p.endDay ,c.photo1,c.photo2,c.photo3
-		from bicycle b, possible_day p, bicycle_photo c  
-		where b.address like '%' ||'판교'|| '%' and b.bicycleNo=p.bicycleNo and to_date('2017-05-28')>=p.startDay and to_date('2017-05-29')<=p.endDay and b.bicycleNo=c.bicycleNo
- 	
-=======
+
 
 	select b.bicycleNo, b.memberId ,b.address ,b.purchasePrice ,b.rentPrice , b.detail ,b.categoryNo,p.startDay,p.endDay ,c.photo1,c.photo2,c.photo3
 		from bicycle b, possible_day p, bicycle_photo c  
@@ -145,9 +140,6 @@ where address like '%'||'판'||'%' and b.bicycleNo=p.bicycleNo and to_date('2017
 
 --좌표까지 조회
 
-
-
->>>>>>> branch 'master' of https://github.com/ReBicycle/finalProject_RB.git
 
 
 ------------종봉----------------------------------------------

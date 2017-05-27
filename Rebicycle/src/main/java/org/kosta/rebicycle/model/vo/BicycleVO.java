1c6b5ef377d87bc0ef6d4c0f97e47e0f74acf2e6
sy2399 <ter194@ajou.ac.kr>
2017-05-27 17:11:16 +0900

    bicycleVO 수정

--- Rebicycle/src/main/java/org/kosta/rebicycle/model/vo/BicycleVO.java
+++ Rebicycle/src/main/java/org/kosta/rebicycle/model/vo/BicycleVO.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class BicycleVO {
 	private int bicycleNo;
-	private String memberId;
+	/*private String memberId;*/
 	private MemberVO memberVO; //자전거 소유인
 	private List<CalendarVO> possibleList; //대여가능일
 	
@@ -16,16 +16,21 @@ public class BicycleVO {
 	private String detail;//상세정보
 	private String address; //자전거 위치 주소
 	
-	//위치VO?
+	private MapVO mapVO;
+	/*//위치VO?
 	private String latitude; //자전거 위치 위도
 	private String longitude; //자전거 위치 경도
+	*/
 	
 	private List<MultipartFile> file;//걍 파일
 	
+	private PhotoVO photoVO;
+	/*
 	private String photo1;//db에 넣을 파일명
 	private String photo2;
 	private String photo3;
-	
+	*/
+	private CategoryVO categoryVO;
 	/*
 	//categoryVO
 	private int categoryNo;

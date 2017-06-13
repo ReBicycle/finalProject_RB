26eb239f9eb1028b1016954962d75c8921ba37bc
KOSTA <KOSTA@192.168.0.122>
2017-06-13 11:28:45 +0900

    ㅇㅇ

--- Rebicycle/src/main/resources/info/rebicycle.sql
+++ Rebicycle/src/main/resources/info/rebicycle.sql
@@ -104,7 +104,6 @@ create table map(
 create table rent(
    rentNo number primary key,
    renterId varchar2(100) not null constraint fk_renter_id references rb_member(id),
-   bicycleNo number not null constraint fk_bicycle_no_deal references bicycle(bicycleNo),
    startDay date not null,
    endDay date not null,
    state number default 0
